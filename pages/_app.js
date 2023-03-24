6c64a4601d7adf92da71037e11dbca78e3d5736d
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-03-24 18:25:34 +0530

    Update _app.js

---
+++ pages/_app.js
@@ -0,0 +1,68 @@
+import Navbar from "@/components/Navbar";
+import "@/styles/globals.css";
+import Footer from "@/components/Footer";
+import { useState,useEffect } from "react";
+
+export default function App({ Component, pageProps }) {
+  
+  const [cart, setcart] = useState({});
+  const [subtotal, setsubtotal] = useState(0);
+  useEffect(() => {
+    try {
+      if(localStorage.getItem('cart')){
+        setcart(JSON.parse(localStorage.getItem('cart')))
+        saveCart(JSON.parse(localStorage.getItem('cart')))
+      }
+    } catch (error) {
+      console.error(error)
+      localStorage.clear()
+    }
+  }, []);
+  
+  const saveCart = (mycart)=>{
+    localStorage.setItem("cart",JSON.stringify(mycart))
+    let subt=0
+    let keys=Object.keys(mycart)
+    for(let i=0; i<keys.length;i++){
+      console.log(keys)
+      subt+=mycart[keys[i]].price*mycart[keys[i]].qty
+    }
+    setsubtotal(subt)
+  }
+  
+  const addToCart = (itemcode,qty,price,name,size,variant)=>{
+    let newcart = cart
+    if(itemcode in cart){
+      newcart[itemcode].qty = cart[itemcode].qty+qty
+    }else{
+      newcart[itemcode]={qty:1,price,name,size,variant}
+    }
+    console.log(newcart)
+    setcart(newcart)
+    saveCart(newcart)
+  }
+
+  const clearCart = () =>{
+    setcart({})
+    saveCart({})
+  }
+
+  const removeFromCart = (itemcode,qty,price,name,size,variant)=>{
+    let newcart = cart
+    if(itemcode in cart){
+      newcart[itemcode].qty = cart[itemcode].qty - qty
+    }
+    if(newcart[itemcode].qty<=0){
+      delete newcart[itemcode]
+    }
+    setcart(newcart)
+    saveCart(newcart)
+  }
+  return (
+    <div>
+      <Navbar  cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subtotal={subtotal}/>
+      <Component cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subtotal={subtotal} {...pageProps} />
+      <Footer />
+    </div>
+  );
+}
--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import Navbar from '@/components/Navbar'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Footer from '@/components/Footer'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <div><Navbar/><Component {...pageProps} /><Footer/></div>
-}
