bf18bbe387472b7643946947b2a5c2a78c3e805d
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-03-26 00:06:30 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -16,7 +16,24 @@ const Footer = () => {
     </div>
     <div class="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
       <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SHOP</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <Link legacyBehavior href={'/tshirts'}><a class="text-gray-600 hover:text-gray-800">T Shirts</a></Link>
+          </li>
+          <li>
+            <Link legacyBehavior href={'/hoodies'}><a class="text-gray-600 hover:text-gray-800">Hoodies</a></Link>
+          </li>
+          <li>
+           <Link legacyBehavior href={'/stickers'}><a class="text-gray-600 hover:text-gray-800">Stickers</a></Link>
+          </li>
+          <li>
+            <Link legacyBehavior href={'/mugs'}><a class="text-gray-600 hover:text-gray-800">Mugs</a></Link>
+          </li>
+        </nav>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">POLICY</h2>
         <nav class="list-none mb-10">
           <li>
             <a class="text-gray-600 hover:text-gray-800">First Link</a>
@@ -33,24 +50,7 @@ const Footer = () => {
         </nav>
       </div>
       <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-        <nav class="list-none mb-10">
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">First Link</a>
-          </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Second Link</a>
-          </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Third Link</a>
-          </li>
-          <li>
-            <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-          </li>
-        </nav>
-      </div>
-      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CINTACT</h2>
         <nav class="list-none mb-10">
           <li>
             <a class="text-gray-600 hover:text-gray-800">First Link</a>
@@ -88,7 +88,7 @@ const Footer = () => {
   <div class="bg-gray-100">
     <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
       <p class="text-gray-500 text-sm text-center sm:text-left">© 2023 Styles Wear —
-        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@Aditya</a>
+        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">All Rights Reserved</a>
       </p>
       <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
         <a class="text-gray-500">
