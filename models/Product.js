4176ff20a1122c61a775b70e5806f727524210c0
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-03-26 04:08:25 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const UsertSchema = new mongose.Schema({
+const ProductSchema = new mongoose.Schema({
     title:{type:String,required:true},
     slug:{type:String,required:true,unique:true},
     desc:{type:String,required:true},
@@ -12,4 +12,5 @@ const UsertSchema = new mongose.Schema({
     availableQty:{type:Number,required:true},
 },{timestamps:true})
 
-export default mongoose.model("Product",UserSchema);
+mongoose.models={}
+export default mongoose.model("Product",ProductSchema);
