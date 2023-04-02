0251d86adacd3e31670e5ae9cca28b9e27e0d074
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-02 18:29:09 +0530

    Update mongoose.js

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -5,7 +5,7 @@ const connectDb = handler => async(req,res) =>{
         return handler(req,res)
     }
     // await mongoose.connect('mongodb+srv://aditya:aditya@cluster0.ypoa7js.mongodb.net/styleswearretryWrites=true&w=majority')
-    await mongoose.connect('mongodb+srv://aditya:aditya@cluster0.ypoa7js.mongodb.net/ecommerceretryWrites=true&w=majority')
+    await mongoose.connect(`${process.env.NEXT_PUBLIC_MONGO_URI}`)
     return handler(req,res)
 }
 
