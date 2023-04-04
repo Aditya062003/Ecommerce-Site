71e45822b67d82cbede25e69d255e88432678b31
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-04 22:05:40 +0530

    committed myorders page

---
+++ pages/api/myorders.js
@@ -0,0 +1,14 @@
+import connectDb from "../../middleware/mongoose";
+import Order from "../../models/Order";
+import jsonwebtoken from "jsonwebtoken"
+
+const handler = async (req, res) => {
+    const token = req.body.token
+    console.log('token',token)
+    const data = jsonwebtoken.verify(token,"aditya")
+    console.log('data',data)
+    let orders = await Order.find({email:data.email})
+    res.status(200).json({orders})
+};
+
+export default connectDb(handler);
