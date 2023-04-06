b64e50bd92931d02c5cdc4de37414939a1f17905
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-06 23:10:08 +0530

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -13,7 +13,7 @@ const handler = async (req, res) => {
     if (user) {
       if (req.body.email == user.email && req.body.password == decryptedPass){
           var token = jwt.sign({ email: user.email, name: user.name }, 'aditya',{expiresIn:'1d'});
-          res.status(200).json({success: true,token});
+          res.status(200).json({success: true,token,email:user.email});
         } 
         else {
           res.status(400).json({ success: false, error: "Invalid Credentials" });
