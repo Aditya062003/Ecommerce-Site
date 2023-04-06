d7590c0be98875281c42c252c35d8818ccbaac80
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-06 23:09:06 +0530

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -7,7 +7,8 @@ const OrderSchema = new mongoose.Schema({
     products:{type:Object,required:true},
     address:{type:String,required:true},
     amount:{type:Number,required:true},
-    status:{type:String,default:'Pending',required:true}
+    status:{type:String,default:'Pending',required:true},
+    deliveryStatus:{type:String,default:'unshipped',required:true}
 },{timestamps:true})
 
 mongoose.models={}
