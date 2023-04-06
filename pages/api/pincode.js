ba87ec31ff8968772627c85f9f365730ea93cc96
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-06 23:10:53 +0530

    Update pincode.js

--- pages/api/pincode.js
+++ pages/api/pincode.js
@@ -1,3 +1,10 @@
 export default function handler(req,res) {
-    res.status(200).json([ 410206,410300,540907,300004,234400 ])
+  let pincodes = {
+    "503302":["Nizamabad","Andhra Pradesh"],
+    "531035":["Visakhapatnam","Andhra Pradesh"],
+    "110092":["East Delhi","Delhi"],
+    "413114":["Pune","Maharastra"],
+    "410206":["Panvel","Maharastra"]
+  }
+    res.status(200).json(pincodes)
   }
