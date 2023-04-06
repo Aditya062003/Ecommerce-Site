07271ab46a837d040c6da280e7e136b45bef2a21
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-06 23:11:19 +0530

    Create posttransaction.js

--- pages/api/posttransaction.js
+++ pages/api/posttransaction.js
@@ -1,17 +1,22 @@
 import connectDb from "../../middleware/mongoose";
 import Order from "../../models/Order";
+import Product from "../../models/Product";
 
 const handler = async (req, res) => {
   let order = await Order.findOneAndUpdate(
     { orderID: req.body.oid },
     { status: "Paid", paymentinfo: JSON.stringify(req.body) }
   );
+  let products = order.products
+  for (let slug in products){
+    await Product.findOneAndUpdate({slug:slug},{$inc:{"availableQty": -products[slug].qty }})
+  }
   // let order = await Order.findOne({orderID:req.body.oid})
   console.log("post oid", req.body.oid);
 
   // Order.findByIdAndUpdate(order._id,{status : 'Paid'})
   // res.status(200).json({body:req.body})
-  res.redirect("/order?id="+order._id, 200);
+  res.redirect("/order?clearcart=1&id="+order._id, 200);
 };
 
 export default connectDb(handler);
