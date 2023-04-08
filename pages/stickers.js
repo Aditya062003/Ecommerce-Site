93b3bf719a98c78a83f4bca71dc8c18cce62e6f7
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-08 22:47:09 +0530

    Update stickers.js

--- pages/stickers.js
+++ pages/stickers.js
@@ -3,54 +3,100 @@ import React from "react";
 import Product from "../models/Product";
 import connectDb from "../middleware/mongoose";
 import mongoose from "mongoose";
+import Head from "next/head";
 
 const stickers = ({ products }) => {
-  console.log(products);
+  // console.log(products);
   return (
-    <section class="text-gray-600  body-font">
-      <div class="container py-12 m-auto ">
-        <div class="flex flex-wrap justify-center">
-        {Object.keys(products).length===0 && <p>Sorry, all stickers are out of stock. New stock coming soon...Stay Tuned!</p>}
-          {Object.keys(products).map((item) => {
-            return (
-              <Link key={item} legacyBehavior href={`/product/${products[item].slug}`}>
-                <div class="lg:w-1/5 cursor-pointer m-4 shadow-md hover:shadow-xl md:w-1/2 p-4 w-full">
-                  <a class="block relative rounded overflow-hidden">
-                    <img
-                      alt="ecommerce"
-                      class="h-[36vh] m-auto"
-                      src={products[item].img}
-                    />
-                  </a>
-                  <div class="mt-4 text-center">
-                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
-                      {products[item].category}
-                    </h3>
-                    <h2 class="text-gray-900 title-font text-lg font-medium">
-                      {products[item].title}
-                    </h2>
-                    <p class="mt-1">₹{products[item].price}</p>
-                    <div class="mt-1">
-                      {products[item].size.includes('S') && <span className='mx-1 py-2 hover:text-white hover:bg-gray-700 px-1 border border-gray-300'>S</span>}
-                      {products[item].size.includes('M') && <span className='mx-1 px-2 py-1 hover:text-white hover:bg-gray-700 border border-gray-300'>M</span>}
-                      {products[item].size.includes('L') && <span className='mx-1 px-2 border py-1 hover:text-white hover:bg-gray-700 border-gray-300'>L</span>}
-                      {products[item].size.includes('XL') && <span className='mx-1 px-2 border py-1 hover:text-white hover:bg-gray-700 border-gray-300'>XL</span>}
-                      {products[item].size.includes('XXL') && <span className='mx-1 px-2 border py-1 hover:text-white hover:bg-gray-700 border-gray-300'>XXL</span>}
-                    </div>
-                    <div className="mt-1">
-                    {products[item].color.includes('Black') && <button class="border-2 border-gray-300 ml-1 bg-black rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {products[item].color.includes('Blue') && <button class="border-2 border-gray-300 ml-1 bg-blue-700 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {products[item].color.includes('Red') && <button class="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {products[item].color.includes('Green') && <button class="border-2 border-gray-300 ml-1 bg-green-600 rounded-full w-6 h-6 focus:outline-none"></button>}
+    <div>
+    <Head>
+        <title>Care-Leisure.com-Wear the style</title>
+        <meta name="description" content="StylesWear" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section className="text-gray-600 min-h-screen body-font">
+        <div class="container  py-12 m-auto ">
+          <div class="flex flex-wrap mx-5 justify-center">
+            {Object.keys(products).length === 0 && (
+              <p>
+                Sorry, all stickers are out of stock. New stock coming
+                soon...Stay Tuned!
+              </p>
+            )}
+            {Object.keys(products).map((item) => {
+              return (
+                <Link
+                  key={item}
+                  legacyBehavior
+                  href={`/product/${products[item].slug}`}
+                >
+                  <div class="lg:w-1/5 cursor-pointer m-4 shadow-md hover:shadow-xl md:w-1/2 p-4 w-full">
+                    <a class="block relative rounded overflow-hidden">
+                      <img
+                        alt="ecommerce"
+                        class="h-[36vh] m-auto"
+                        src={products[item].img}
+                      />
+                    </a>
+                    <div class="mt-4 text-center">
+                      <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                        {products[item].category}
+                      </h3>
+                      <h2 class="text-gray-900 title-font text-lg font-medium">
+                        {products[item].title}
+                      </h2>
+                      <p class="mt-1">₹{products[item].price}</p>
+                      <div class="mt-1">
+                        {products[item].size.includes("S") && (
+                          <span className="mx-1 py-2 hover:text-white hover:bg-gray-700 px-1 border border-gray-300">
+                            S
+                          </span>
+                        )}
+                        {products[item].size.includes("M") && (
+                          <span className="mx-1 px-2 py-1 hover:text-white hover:bg-gray-700 border border-gray-300">
+                            M
+                          </span>
+                        )}
+                        {products[item].size.includes("L") && (
+                          <span className="mx-1 px-2 border py-1 hover:text-white hover:bg-gray-700 border-gray-300">
+                            L
+                          </span>
+                        )}
+                        {products[item].size.includes("XL") && (
+                          <span className="mx-1 px-2 border py-1 hover:text-white hover:bg-gray-700 border-gray-300">
+                            XL
+                          </span>
+                        )}
+                        {products[item].size.includes("XXL") && (
+                          <span className="mx-1 px-2 border py-1 hover:text-white hover:bg-gray-700 border-gray-300">
+                            XXL
+                          </span>
+                        )}
+                      </div>
+                      <div className="mt-1">
+                        {products[item].color.includes("Black") && (
+                          <button class="border-2 border-gray-300 ml-1 bg-black rounded-full w-6 h-6 focus:outline-none"></button>
+                        )}
+                        {products[item].color.includes("Blue") && (
+                          <button class="border-2 border-gray-300 ml-1 bg-blue-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                        )}
+                        {products[item].color.includes("Red") && (
+                          <button class="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                        )}
+                        {products[item].color.includes("Green") && (
+                          <button class="border-2 border-gray-300 ml-1 bg-green-600 rounded-full w-6 h-6 focus:outline-none"></button>
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Link>
-            );
-          })}
+                </Link>
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
@@ -81,6 +127,9 @@ export async function getServerSideProps(context) {
       if (item.availableQty > 0) {
         stickers[item.title].color = [item.color];
         stickers[item.title].size = [item.size];
+      } else {
+        stickers[item.title].color = [];
+        stickers[item.title].size = [];
       }
     }
   }
@@ -91,17 +140,17 @@ export async function getServerSideProps(context) {
 
 export default stickers;
 
-      //   <Link legacyBehavior href={'/'}>
-        
-      //   <div class="lg:w-1/5 cursor-pointer m-4 shadow-md hover:shadow-xl md:w-1/2 p-4 w-full">
-      //     <a class="block relative rounded overflow-hidden">
-      //       <img alt="ecommerce" class=" h-[36vh] m-auto" src="https://m.media-amazon.com/images/I/51yrBJHE+WL._AC_UL480_FMwebp_QL65_.jpg"/>
-      //     </a>
-      //     <div class=" text-center mt-4">
-      //       <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-      //       <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-      //       <p class="mt-1">₹499</p>
-      //       <p class="mt-1"> S M L XL XXL</p>
-      //     </div>
-      //   </div>
-      // </Link>
+//   <Link legacyBehavior href={'/'}>
+
+//   <div class="lg:w-1/5 cursor-pointer m-4 shadow-md hover:shadow-xl md:w-1/2 p-4 w-full">
+//     <a class="block relative rounded overflow-hidden">
+//       <img alt="ecommerce" class=" h-[36vh] m-auto" src="https://m.media-amazon.com/images/I/51yrBJHE+WL._AC_UL480_FMwebp_QL65_.jpg"/>
+//     </a>
+//     <div class=" text-center mt-4">
+//       <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+//       <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+//       <p class="mt-1">₹499</p>
+//       <p class="mt-1"> S M L XL XXL</p>
+//     </div>
+//   </div>
+// </Link>
