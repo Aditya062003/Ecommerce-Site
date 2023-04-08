4962e8d529be49ba90721909ad0c2803303b6a12
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-08 19:43:10 +0530

    some changes in myorders page

--- pages/api/myorders.js
+++ pages/api/myorders.js
@@ -1,14 +1,14 @@
 import connectDb from "../../middleware/mongoose";
 import Order from "../../models/Order";
-import jsonwebtoken from "jsonwebtoken"
+import jsonwebtoken from "jsonwebtoken";
 
 const handler = async (req, res) => {
-    const token = req.body.token
-    console.log('token',token)
-    const data = jsonwebtoken.verify(token,"aditya")
-    console.log('data',data)
-    let orders = await Order.find({email:data.email,status:'Paid'})
-    res.status(200).json({orders})
+  const token = req.body.token;
+
+  const data = jsonwebtoken.verify(token, "aditya");
+
+  let orders = await Order.find({ email: data.email, status: "Paid" });
+  res.status(200).json({ orders });
 };
 
 export default connectDb(handler);
