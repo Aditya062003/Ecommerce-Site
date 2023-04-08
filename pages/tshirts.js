abf10e959fa9b1767d5e778201c017f817f090a5
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-08 22:47:40 +0530

    Update tshirts.js

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -3,16 +3,33 @@ import React from "react";
 import Product from "../models/Product";
 import connectDb from "../middleware/mongoose";
 import mongoose from "mongoose";
+import Head from "next/head";
 
 const Tshirts = ({ products }) => {
-  console.log(products);
+  // console.log(products);
   return (
-    <section class="text-gray-600  body-font">
+    <section className="text-gray-600 min-h-screen body-font">
+      <Head>
+        <title>Care-Leisure.com-Wear the style</title>
+        <meta name="description" content="StylesWear" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div class="container py-12 m-auto ">
-        <div class="flex flex-wrap justify-center">
+        <div class="flex flex-wrap mx-5 justify-center">
+          {Object.keys(products).length === 0 && (
+            <p>
+              Sorry, all hoodies are out of stock. New stock coming soon...Stay
+              Tuned!
+            </p>
+          )}
           {Object.keys(products).map((item) => {
             return (
-              <Link key={item} legacyBehavior href={`/product/${products[item].slug}`}>
+              <Link
+                key={item}
+                legacyBehavior
+                href={`/product/${products[item].slug}`}
+              >
                 <div class="lg:w-1/5 cursor-pointerfhost m-4 shadow-md hover:shadow-xl md:w-1/2 p-4 w-full">
                   <a class="block relative rounded overflow-hidden">
                     <img
@@ -30,17 +47,45 @@ const Tshirts = ({ products }) => {
                     </h2>
                     <p class="mt-1">₹{products[item].price}</p>
                     <div class="mt-1">
-                      {products[item].size.includes('S') && <span className='mx-1 py-2 hover:text-white hover:bg-gray-700 px-1 border border-gray-300'>S</span>}
-                      {products[item].size.includes('M') && <span className='mx-1 px-2 py-1 hover:text-white hover:bg-gray-700 border border-gray-300'>M</span>}
-                      {products[item].size.includes('L') && <span className='mx-1 px-2 border py-1 hover:text-white hover:bg-gray-700 border-gray-300'>L</span>}
-                      {products[item].size.includes('XL') && <span className='mx-1 px-2 border py-1 hover:text-white hover:bg-gray-700 border-gray-300'>XL</span>}
-                      {products[item].size.includes('XXL') && <span className='mx-1 px-2 border py-1 hover:text-white hover:bg-gray-700 border-gray-300'>XXL</span>}
+                      {products[item].size.includes("S") && (
+                        <span className="mx-1 py-2 hover:text-white hover:bg-gray-700 px-1 border border-gray-300">
+                          S
+                        </span>
+                      )}
+                      {products[item].size.includes("M") && (
+                        <span className="mx-1 px-2 py-1 hover:text-white hover:bg-gray-700 border border-gray-300">
+                          M
+                        </span>
+                      )}
+                      {products[item].size.includes("L") && (
+                        <span className="mx-1 px-2 border py-1 hover:text-white hover:bg-gray-700 border-gray-300">
+                          L
+                        </span>
+                      )}
+                      {products[item].size.includes("XL") && (
+                        <span className="mx-1 px-2 border py-1 hover:text-white hover:bg-gray-700 border-gray-300">
+                          XL
+                        </span>
+                      )}
+                      {products[item].size.includes("XXL") && (
+                        <span className="mx-1 px-2 border py-1 hover:text-white hover:bg-gray-700 border-gray-300">
+                          XXL
+                        </span>
+                      )}
                     </div>
                     <div className="mt-1">
-                    {products[item].color.includes('Black') && <button class="border-2 border-gray-300 ml-1 bg-black rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {products[item].color.includes('Blue') && <button class="border-2 border-gray-300 ml-1 bg-blue-700 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {products[item].color.includes('Red') && <button class="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {products[item].color.includes('Green') && <button class="border-2 border-gray-300 ml-1 bg-green-600 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes("Black") && (
+                        <button class="border-2 border-gray-300 ml-1 bg-black rounded-full w-6 h-6 focus:outline-none"></button>
+                      )}
+                      {products[item].color.includes("Blue") && (
+                        <button class="border-2 border-gray-300 ml-1 bg-blue-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                      )}
+                      {products[item].color.includes("Red") && (
+                        <button class="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                      )}
+                      {products[item].color.includes("White") && (
+                        <button class="border-2 border-gray-400 ml-1 bg-white  rounded-full w-6 h-6 focus:outline-none"></button>
+                      )}
                     </div>
                   </div>
                 </div>
@@ -80,6 +125,9 @@ export async function getServerSideProps(context) {
       if (item.availableQty > 0) {
         tshirts[item.title].color = [item.color];
         tshirts[item.title].size = [item.size];
+      } else {
+        tshirts[item.title].color = [];
+        tshirts[item.title].size = [];
       }
     }
   }
@@ -90,12 +138,3 @@ export async function getServerSideProps(context) {
 
 export default Tshirts;
 
-// "title":"cool tshirt",
-//     "slug":"cool-tshirt",
-//     "desc":"desc",
-//     "img":"img",
-//     "category":"tshirt",
-//     "size":"M",
-//     "color":"Red",
-//     "price":699,
-//     "availableQty":1
