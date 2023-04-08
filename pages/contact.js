b13a8e288ca5ab48c6d10c51343b09fb34574263
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-08 22:42:56 +0530

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Head from "next/head";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="min-h-screen">
+      <Head>
+        <title>Care-Leisure.com-Wear the style</title>
+        <meta name="description" content="StylesWear" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Contact
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
