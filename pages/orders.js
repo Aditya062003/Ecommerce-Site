59ce7c71e594709add6916c97df53d1d101e5418
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-08 22:46:07 +0530

    Update orders.js

--- pages/orders.js
+++ pages/orders.js
@@ -1,24 +1,49 @@
-import {React,useEffect} from "react";
+import { React, useEffect } from "react";
 import mongoose from "mongoose";
-import Order from "../models/order";
+import Order from "../models/Order";
 import { useRouter } from "next/router";
-
+import Link from "next/link";
+import { useState } from "react";
+import Head from "next/head";
 
 const Orders = () => {
-    const router = useRouter()
-    useEffect(() => {
-        if(!localStorage.getItem('token')){
-            router.push('/')
-        }
-    }, []);
+  const router = useRouter();
+  const [orders, setorders] = useState([]);
+  useEffect(() => {
+    const fetchOrders = async () => {
+      let a = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/myorders`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: JSON.parse(localStorage.getItem("myuser")).token,
+        }),
+      });
+      let res = await a.json();
+
+      setorders(res.orders);
+    };
+
+    fetchOrders();
+  }, []);
+
   return (
-    <div>
+    <div className="min-h-screen">
+      <Head>
+        <title>Care-Leisure.com-Wear the style</title>
+        <meta name="description" content="StylesWear" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="container mx-auto">
         <div class="flex flex-col">
           <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
             <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
               <div class="overflow-hidden">
-                <h1 className="font-bold py-2 md:px-2 md:text-left text-center  text-xl">My Orders</h1>
+                <h1 className="font-bold py-2 md:px-2 md:text-left text-center  text-xl">
+                  My Orders
+                </h1>
                 <table class="min-w-full">
                   <thead class="bg-white border-b">
                     <tr>
@@ -26,99 +51,49 @@ const Orders = () => {
                         scope="col"
                         class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
                       >
-                        #
+                        #OrderID
                       </th>
                       <th
                         scope="col"
                         class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
                       >
-                        First
+                        Email
                       </th>
                       <th
                         scope="col"
                         class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
                       >
-                        Last
+                        Amount
                       </th>
                       <th
                         scope="col"
                         class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
                       >
-                        Handle
+                        Details
                       </th>
                     </tr>
                   </thead>
                   <tbody>
-                    <tr class="bg-gray-100 border-b">
-                      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        1
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        Mark
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        Otto
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        @mdo
-                      </td>
-                    </tr>
-                    <tr class="bg-white border-b">
-                      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        2
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        Jacob
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        Dillan
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        @fat
-                      </td>
-                    </tr>
-                    <tr class="bg-gray-100 border-b">
-                      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        3
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        Mark
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        Twen
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        @twitter
-                      </td>
-                    </tr>
-                    <tr class="bg-white border-b">
-                      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        4
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        Bob
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        Dillan
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        @fat
-                      </td>
-                    </tr>
-                    <tr class="bg-gray-100 border-b">
-                      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        5
-                      </td>
-                      <td
-                        colspan="2"
-                        class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center"
-                      >
-                        Larry the Bird
-                      </td>
-                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        @twitter
-                      </td>
-                    </tr>
+                    {orders.map((item) => {
+                      return (
+                        <tr key={item._id} className="bg-gray-100 border-b">
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            {item.orderID}
+                          </td>
+                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            {item.email}
+                          </td>
+                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            {item.amount}
+                          </td>
+                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            <Link legacyBehavior href={"/order?id=" + item._id}>
+                              <a>Details</a>
+                            </Link>
+                          </td>
+                        </tr>
+                      );
+                    })}
                   </tbody>
                 </table>
               </div>
@@ -129,17 +104,5 @@ const Orders = () => {
     </div>
   );
 };
-export async function getServerSideProps(context) {
-    if (!mongoose.connections[0].readyState) {
-      await mongoose.connect(
-        "mongodb+srv://aditya:aditya@cluster0.ypoa7js.mongodb.net/ecommerceretryWrites=true&w=majority"
-      );
-    }
-    let orders = await Order.find({  });
-  
-    return {
-      props: {orders:orders},
-    };
-  }
 
 export default Orders;
