1b8eed18fe436c465bd7696256db4acb6e7863de
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-08 19:38:54 +0530

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,15 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const OrderSchema = new mongoose.Schema({
-    email:{type:String,required:true},
-    orderID:{type:String,required:true},
-    paymentinfo:{type:String,default:''},
-    products:{type:Object,required:true},
-    address:{type:String,required:true},
-    amount:{type:Number,required:true},
-    status:{type:String,default:'Pending',required:true},
-    deliveryStatus:{type:String,default:'unshipped',required:true}
-},{timestamps:true})
+const OrderSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    orderID: { type: String, required: true },
+    paymentinfo: { type: String, default: "" },
+    products: { type: Object, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    phone: { type: String, required: true },
+    name: { type: String, required: true },
+    transactionId: { type: String },
+    amount: { type: Number, required: true },
+    status: { type: String, default: "Pending", required: true },
+    deliveryStatus: { type: String, default: "unshipped", required: true },
+  },
+  { timestamps: true }
+);
 
-mongoose.models={}
-export default mongoose.model("Order",OrderSchema);
+mongoose.models = {};
+export default mongoose.model("Order", OrderSchema);
