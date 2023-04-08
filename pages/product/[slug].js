f9f4ef2016c46802c93f084c0cd0419826e02551
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-08 19:47:55 +0530

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,12 +1,12 @@
 import { useRouter } from "next/router";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import mongoose from "mongoose";
 import Product from "../../models/Product";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Error from "next/error";
 
-const Post = ({ buyNow, addToCart, product, variants }) => {
-  console.log(product, variants);
+const Post = ({ buyNow, addToCart, product, variants, error }) => {
   const router = useRouter();
   const { slug } = router.query;
   const [pin, setpin] = useState();
@@ -34,33 +34,36 @@ const Post = ({ buyNow, addToCart, product, variants }) => {
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
-        draggable: true,
+        draggable: true,  
         progress: undefined,
         theme: "light",
       });
     }
   };
-  const [color, setcolor] = useState(product.color);
-  const [size, setsize] = useState(product.size); 
+  const [color, setcolor] = useState();
+  const [size, setsize] = useState();
   useEffect(() => {
-    setcolor(product.color)
-    setsize(product.size)
-  }, [router.query])
-  
+    if (!error) {
+      setcolor(product.color);
+      setsize(product.size);
+    }
+  }, [router.query]);
 
   const onChangePin = (e) => {
     setpin(e.target.value);
   };
 
-  
   const refreshVariants = (newsize, newcolor) => {
     let url = `${process.env.NEXT_PUBLIC_HOST}/product/${variants[newcolor][newsize]["slug"]}`;
-    router.push(url)
+    router.push(url);
   };
+  if (error == 404) {
+    return <Error statusCode={404} />;
+  }
 
   return (
     <>
-      <section class="text-gray-600 body-font overflow-hidden">
+      <section class="text-gray-600 body-font min-h-screen overflow-hidden">
         <ToastContainer />
         <div class="container px-5 py-12 mx-auto">
           <div class="lg:w-4/5 mx-auto flex flex-wrap">
@@ -76,104 +79,7 @@ const Post = ({ buyNow, addToCart, product, variants }) => {
               <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">
                 {product.title}({product.size}/{product.color})
               </h1>
-              {/* <div class="flex mb-4">
-                <span class="flex items-center">
-                  <svg
-                    fill="currentColor"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-4 h-4 text-indigo-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                  </svg>
-                  <svg
-                    fill="currentColor"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-4 h-4 text-indigo-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                  </svg>
-                  <svg
-                    fill="currentColor"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-4 h-4 text-indigo-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                  </svg>
-                  <svg
-                    fill="currentColor"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-4 h-4 text-indigo-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                  </svg>
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-4 h-4 text-indigo-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                  </svg>
-                  <span class="text-gray-600 ml-3">4 Reviews</span>
-                </span>
-                <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                  <a class="text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                    </svg>
-                  </a>
-                  <a class="text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                    </svg>
-                  </a>
-                  <a class="text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
-                  </a>
-                </span>
-              </div> */}
+             
               <p class="leading-relaxed">{product.desc}</p>
               <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                 <div class="flex">
@@ -233,19 +139,19 @@ const Post = ({ buyNow, addToCart, product, variants }) => {
                       }}
                       class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10"
                     >
-                      {Object.keys(variants[color]).includes("S") && (
+                      {color && Object.keys(variants[color]).includes("S") && (
                         <option>S</option>
                       )}
-                      {Object.keys(variants[color]).includes("M") && (
+                      {color && Object.keys(variants[color]).includes("M") && (
                         <option>M</option>
                       )}
-                      {Object.keys(variants[color]).includes("L") && (
+                      {color && Object.keys(variants[color]).includes("L") && (
                         <option>L</option>
                       )}
-                      {Object.keys(variants[color]).includes("XL") && (
+                      {color && Object.keys(variants[color]).includes("XL") && (
                         <option>XL</option>
                       )}
-                      {Object.keys(variants[color]).includes("XXL") && (
+                      {color && Object.keys(variants[color]).includes("XXL") && (
                         <option>XXL</option>
                       )}
                     </select>
@@ -266,10 +172,20 @@ const Post = ({ buyNow, addToCart, product, variants }) => {
                 </div>
               </div>
               <div class="flex">
-                <span class="title-font font-medium text-2xl ml-2 text-gray-900">
-                  ₹{product.price}
-                </span>
+                {product.availableQty > 0 && (
+                  <span class="title-font font-medium text-2xl ml-2 text-gray-900">
+                    {" "}
+                    ₹{product.price}{" "}
+                  </span>
+                )}
+                {product.availableQty <= 0 && (
+                  <span class="title-font font-medium text-2xl ml-2 text-gray-900">
+                    {" "}
+                    Out of Stock!{" "}
+                  </span>
+                )}
                 <button
+                  disabled={product.availableQty <= 0}
                   onClick={() => {
                     buyNow(
                       slug,
@@ -280,11 +196,12 @@ const Post = ({ buyNow, addToCart, product, variants }) => {
                       product.color
                     );
                   }}
-                  class="flex ml-14 text-white bg-indigo-500 border-0 text-sm py-2 px-2 md:px-6 focus:outline-none hover:bg-indigo-600 rounded"
+                  class="disabled:bg-indigo-300 flex ml-14 text-white bg-indigo-500 border-0 text-sm py-2 px-2 md:px-6 focus:outline-none hover:bg-indigo-600 rounded"
                 >
                   Buy Now
                 </button>
                 <button
+                  disabled={product.availableQty <= 0}
                   onClick={() => {
                     addToCart(
                       slug,
@@ -295,22 +212,10 @@ const Post = ({ buyNow, addToCart, product, variants }) => {
                       product.color
                     );
                   }}
-                  class="flex ml-6 text-white bg-indigo-500 border-0 text-sm py-2 px-2 md:px-6 focus:outline-none hover:bg-indigo-600 rounded"
+                  class=" disabled:bg-indigo-300 flex ml-6 text-white bg-indigo-500 border-0 text-sm py-2 px-2 md:px-6 focus:outline-none hover:bg-indigo-600 rounded"
                 >
                   Add to Cart
                 </button>
-                {/* <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                  </svg>
-                </button> */}
               </div>
               <div className="flex mt-6 pin">
                 <div class="relative">
@@ -354,7 +259,13 @@ export async function getServerSideProps(context) {
       "mongodb+srv://aditya:aditya@cluster0.ypoa7js.mongodb.net/ecommerceretryWrites=true&w=majority"
     );
   }
+  let error=null;
   let product = await Product.findOne({ slug: context.query.slug });
+  if (product == null) {
+    return {
+      props: { error: 404 },
+    };
+  }
   let variants = await Product.find({ title: product.title });
   let colorSizeSlug = {};
   for (let item of variants) {
@@ -368,6 +279,7 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
+      error: error,
       product: JSON.parse(JSON.stringify(product)),
       variants: JSON.parse(JSON.stringify(colorSizeSlug)),
     },
