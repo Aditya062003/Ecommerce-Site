577d47055744f9c0fcf3141e84b49836e8ad7371
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-11 00:54:44 +0530

    Update index.js

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,8 +1,8 @@
 import { Grid } from "@mui/material";
-import BlogCard from "../../src/components/dashboard/BlogCard";
+
 import SalesOverview from "../../src/components/dashboard/SalesOverview";
 import DailyActivity from "../../src/components/dashboard/DailyActivity";
-import ProductPerfomance from "../../src/components/dashboard/AllProducts";
+
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../../src/theme/theme";
 import FullLayout from "../../src/layouts/FullLayout";
@@ -24,12 +24,8 @@ export default function Index() {
           <Grid item xs={12} lg={4}>
             <DailyActivity />
           </Grid>
-          <Grid item xs={12} lg={8}>
-            <ProductPerfomance />
-          </Grid>
-          <Grid item xs={12} lg={12}>
-            <BlogCard />
-          </Grid>
+          <Grid item xs={12} lg={8}></Grid>
+          <Grid item xs={12} lg={12}></Grid>
         </Grid>
       </FullLayout>
     </ThemeProvider>
