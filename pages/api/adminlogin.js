7665e8ab17ba261aa598e0c79ec0f976f246a73e
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-11 14:48:03 +0530

    Add files via upload

---
+++ pages/api/adminlogin.js
@@ -0,0 +1,41 @@
+import User from "../../models/User";
+import connectDb from "../../middleware/mongoose";
+var CryptoJS = require("crypto-js");
+var jwt = require("jsonwebtoken");
+
+const handler = async (req, res) => {
+  if (req.method == "POST") {
+    let user = await User.findOne({ email: req.body.email });
+    var bytes = CryptoJS.AES.decrypt(user.password, "secret123");
+    let decryptedPass = bytes.toString(CryptoJS.enc.Utf8);
+    if (user) {
+      if (req.body.email == user.email && req.body.password == decryptedPass) {
+        var admintoken = jwt.sign(
+          { email: user.email, name: user.name },
+          "aditya",
+          {
+            expiresIn: "1d",
+          }
+        );
+        if (user.admin=="true") {
+          res
+            .status(200)
+            .json({ success: true, admintoken, email: user.email });
+        } else {
+          res
+            .status(400)
+            .json({
+              success: false,
+              error: "Only Admins has access to this page.",
+            });
+        }
+      } else {
+        res.status(400).json({ success: false, error: "Invalid Credentials" });
+      }
+    } else {
+      res.status(400).json({ success: false, error: "User not found!" });
+    }
+  }
+};
+
+export default connectDb(handler);
