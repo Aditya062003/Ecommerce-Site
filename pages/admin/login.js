12ef1bd43d518fc2c68ca50e6a63f298a399a249
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-11 14:46:53 +0530

    Update login.js

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -1,7 +1,145 @@
-import React from "react";
+import React, { useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/router";
 
 const Login = () => {
-  return <div className="min-h-screen">Login</div>;
+  const Router = useRouter()
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const handleChange = (e) => {
+    if (e.target.name == "email") {
+      setemail(e.target.value);
+    } else if (e.target.name == "password") {
+      setpassword(e.target.value);
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = { email, password };
+    let res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/adminlogin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    let response = await res.json();
+
+    setemail("");
+    setpassword("");
+    if (response.success) {
+      localStorage.setItem(
+        "adminuser",
+        JSON.stringify({ admintoken: response.admintoken, email: response.email })
+      );
+
+      toast.success("Admin successfully logged in!", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      setTimeout(() => {
+        Router.push(`${process.env.NEXT_PUBLIC_HOST}/admin`);
+      }, 2000);
+    } else {
+      toast.error(response.error, {
+        position: "top-right",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
+  return (
+    <section className="h-screen m-auto">
+      <Head>
+        <title>Care-Leisure.com-Wear the style</title>
+        <meta name="description" content="StylesWear" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ToastContainer />
+      <div className="container h-full px-6 py-24 m-auto">
+        <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between m-auto">
+          <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
+            <img
+              src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+              className="w-full"
+              alt="Phone image"
+            />
+          </div>
+          <div className="md:w-8/12 lg:ml-6 lg:w-5/12">
+            <form onSubmit={handleSubmit} method="POST">
+              <div class="container flex">
+                <div class="lg:w-3/4 bg-white rounded-lg p-8 flex flex-col mx-auto w-full mt-10 md:mt-0 shadow-xl">
+                  <h1 class="text-gray-900 mb-3 text-2xl text-center font-bold title-font">
+                    Please login to your Account
+                  </h1>
+                  <div class="relative mb-4">
+                    <label for="email" class="leading-7 text-sm text-gray-600">
+                      Email
+                    </label>
+                    <input
+                      onChange={handleChange}
+                      value={email}
+                      type="email"
+                      id="email"
+                      name="email"
+                      class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
+                  <div class="relative mb-4">
+                    <label
+                      for="password"
+                      class="leading-7 text-sm text-gray-600"
+                    >
+                      Password
+                    </label>
+                    <input
+                      onChange={handleChange}
+                      value={password}
+                      type="password"
+                      id="password"
+                      name="password"
+                      class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
+
+                  <button class="text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                    Signin
+                  </button>
+
+                  <div class="flex items-center justify-between mt-6 pb-6">
+                    <p class="mb-0 mr-2">Do not have an account?</p>
+                    <Link href={"/signup"}>
+                      <button
+                        type="button"
+                        className="text-indigo-600 border border-indigo-600 py-2 px-4 focus:outline-none hover:text-white hover:bg-indigo-600 rounded text-lg"
+                      >
+                        Register
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
