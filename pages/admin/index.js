812e3e467d5f7661b2438694c2d9d6f70eb866c8
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-04-11 14:46:30 +0530

    Update index.js

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -2,14 +2,39 @@ import { Grid } from "@mui/material";
 
 import SalesOverview from "../../src/components/dashboard/SalesOverview";
 import DailyActivity from "../../src/components/dashboard/DailyActivity";
-
+import { useEffect } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../../src/theme/theme";
 import FullLayout from "../../src/layouts/FullLayout";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/router";
 
 export default function Index() {
+  const Router = useRouter()
+  useEffect(() => {
+    const adminuser = JSON.parse(localStorage.getItem("adminuser"));
+    if(!adminuser){
+      toast.error("Only admins are authorized to view this page!", {
+        toastId: "error",
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      setTimeout(() => {
+        Router.push(`${process.env.NEXT_PUBLIC_HOST}/admin/login`);
+      }, 1000);
+    }
+  },[])
+  
   return (
     <ThemeProvider theme={theme}>
+    <ToastContainer/>
       <style jsx global>{`
         footer {
           display: none;
